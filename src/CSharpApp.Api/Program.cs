f11f8f5a590e118afffcf2d5e33fc563f01d12c2
Grigoris Loukidis <grlouk@outlook.com>
2024-09-06 16:03:47 +0300

    Added extra endpoints and http client wrapper configuration.

--- src/CSharpApp.Api/Program.cs
+++ src/CSharpApp.Api/Program.cs
@@ -1,3 +1,7 @@
+using CSharpApp.Application.Configuration;
+using CSharpApp.Core.Dtos;
+using CSharpApp.Infrastructure.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger());
@@ -6,6 +10,8 @@ builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
 builder.Services.AddDefaultConfiguration();
+
+builder.Services.AddHttpClient<IHttpClientWrapper, HttpClientWrapper>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,7 +30,7 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/todos", async (ITodoService todoService) =>
     {
-        var todos = await todoService.GetAllTodos();
+        var todos = await todoService.GetAll();
         return todos;
     })
     .WithName("GetTodos")
@@ -32,10 +38,42 @@ app.MapGet("/todos", async (ITodoService todoService) =>
 
 app.MapGet("/todos/{id}", async ([FromRoute] int id, ITodoService todoService) =>
     {
-        var todos = await todoService.GetTodoById(id);
+        var todos = await todoService.GetById(id);
         return todos;
     })
     .WithName("GetTodosById")
+    .WithOpenApi();
+
+app.MapGet("/posts", async (IPostService postService) =>
+{
+    var posts = await postService.GetAll();
+    return posts;
+})
+    .WithName("GetPosts")
+    .WithOpenApi();
+
+app.MapGet("/posts/{id}", async ([FromRoute] int id, IPostService postService) =>
+{
+    var posts = await postService.GetById(id);
+    return posts;
+})
+    .WithName("GetPostsById")
+    .WithOpenApi();
+
+app.MapPost("/posts", async (IPostService postService, PostRecord newPost) =>
+{
+    var createdPost = await postService.Add(newPost);
+    return Results.Created("/posts", createdPost);
+})
+    .WithName("AddPost")
+    .WithOpenApi();
+
+app.MapDelete("/posts/{id}", async ([FromRoute] int id, IPostService postService) =>
+{
+    await postService.Delete(id);
+    return Results.NoContent();
+})
+    .WithName("DeletePost")
     .WithOpenApi();
 
 app.Run();
