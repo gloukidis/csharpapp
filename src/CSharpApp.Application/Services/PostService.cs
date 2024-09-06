00a26c9f8927e12b118eac056989f7623dbc626f
Grigoris Loukidis <grlouk@outlook.com>
2024-09-06 16:01:09 +0300

    Added post service.

---
+++ src/CSharpApp.Application/Services/PostService.cs
@@ -0,0 +1,33 @@
+﻿using CSharpApp.Infrastructure.Http;
+
+namespace CSharpApp.Application.Services;
+
+public class PostService : IPostService
+{
+    private readonly IHttpClientWrapper _httpClientWrapper;
+
+    public PostService(IHttpClientWrapper httpClientWrapper)
+    {
+        _httpClientWrapper = httpClientWrapper;
+    }
+
+    public async Task<int?> Add(PostRecord newPost)
+    {
+        return await _httpClientWrapper.PostAsync<PostRecord, int>("post", newPost);
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        return await _httpClientWrapper.DeleteAsync($"posts/{id}");
+    }
+
+    public async Task<List<PostRecord>?> GetAll()
+    {
+        return await _httpClientWrapper.GetAsync<List<PostRecord>>($"posts");
+    }
+
+    public async Task<PostRecord?> GetById(int id)
+    {
+        return await _httpClientWrapper.GetAsync<PostRecord>($"posts/{id}");
+    }
+}
---
+++ src/CSharpApp.Core/Interfaces/IPostService.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpApp.Core.Interfaces;
+
+public interface IPostService
+{
+    Task<PostRecord?> GetById(int id);
+
+    Task<List<PostRecord>?> GetAll();
+
+    Task<int?> Add(PostRecord newPost);
+
+    Task<bool> Delete(int id);
+}
